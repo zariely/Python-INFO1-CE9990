ae9f3510a824020e9ca304900d17b7799089cc19
zariely <zarielyg@gmail.com>
2017-06-25 15:42:35 -0400

    Update flag.py

--- flag.py
+++ flag.py
@@ -2,6 +2,7 @@
 flag.py
 
 Draws the Dominican Republic flag in color on a tkinter Canvas widget.
+This is a condensed version of the previous flag.py script
 
 '''
 
@@ -10,8 +11,8 @@ import sys
 
 #dimensions of entire flag, in pixels
 
-cross_width = 50
-height = 6 * cross_width  #300px
+cross_width = 60
+height = 6 * cross_width
 width = int(height * 3/2) #450px
 
 #The root widget is the window that will contain everything we draw.
@@ -20,8 +21,8 @@ root.title('Dominican Republic Flag')
 root.geometry(str(width) + 'x' + str(height))
 
 #highlightthickness = 0 allows the canvas to occupy the entire root.
-canvas = tkinter.Canvas(root, highlightthickness = 0)
-
+#also creates a white background
+canvas = tkinter.Canvas(root, highlightthickness = 0, background = '#ffffff')
 
 y = 0
 while y < height:
@@ -29,38 +30,18 @@ while y < height:
     x = 0
     while x < width:
 
-        #top left dark blue
-        #coordinates: 0,0 to ~200,125
-        if x < (width * .444) and y < (2.5 * cross_width):
-            canvas.create_rectangle(x, y, x + 1, y + 1, width = 0, fill = '#002D62')
-            
+        #top left & bottom right rectangles (dark blue)
+        if x < width * .444 and y < 2.5 * cross_width \
+           or x > width * .555 and y > 3.5 * cross_width: 
+            canvas.create_rectangle(x, y, x + 1, y + 1, width = 0, fill = '#002D62')           
 
-        #bottom left red (leaves 50 px of negative space)
-        #coordinates: 0,175 to ~200,300          
-        elif x < width * .444 and y > (3.5 * cross_width):  
-             canvas.create_rectangle(x, y, x + 1, y + 1, width = 0, fill = '#CE1126')
-    
-            
-        #top right red
-        #coordinates: ~250,0 to 450,125
-        elif x > (width * .555) and y < (2.5 * cross_width):
+        #bottom left & top right rectangles (red)           
+        elif x < width * .444 and y > 3.5 * cross_width \
+            or  x > width * .555 and y < 2.5 * cross_width:
             canvas.create_rectangle(x, y, x + 1, y + 1, width = 0, fill = '#CE1126')
-
-        
-        #bottom right dark blue
-        #coordinates: ~250,175 to 450,300
-        elif x > width * .555 and y > (3.5 * cross_width):  
-             canvas.create_rectangle(x, y, x + 1, y + 1, width = 0, fill = '#002D62')
-             
-
-        #cross (negative space)            
-        else:
-            canvas.create_rectangle(x, y, x + 1, y + 1, width = 0, fill = '#ffffff')
 
         x += 1
     y += 1
-
-
 
 #Make the canvas visible by packing it into the root.
 canvas.pack(expand = tkinter.YES, fill = "both")
