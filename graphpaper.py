331846bdeba9688c05b8cbdebaafe4e16c39b255
zariely <zarielyg@gmail.com>
2017-06-29 10:06:21 -0400

    Create graphpaper.py

---
+++ graphpaper.py
@@ -0,0 +1,36 @@
+'''
+graphpaper.py
+ 
+This program prints out a grid based on user input.
+ 
+'''
+ 
+ 
+import sys
+ 
+#variables
+plus = '+'
+dash = '-'
+pipe = '|'
+space = ' '
+
+
+row = int(input('How many rows of boxes? '))
+col = int(input('How many columns of boxes? '))
+width = int(input('How many rows of spaces in each box? '))
+ 
+
+a = plus + (width * dash + plus) * row
+b = pipe + (width * space + pipe) * row
+
+x = 0
+while x < row:
+    print(a)
+    x += 1
+
+    y = 0
+    while y < col:
+        print(b)
+        y += 1
+
+sys.exit(0)
