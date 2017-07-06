34870de3fbfa7df5636a074875e7217d56b204ad
zariely <zarielyg@gmail.com>
2017-07-06 13:43:57 -0400

    Update flag.py

--- flag.py
+++ flag.py
@@ -2,27 +2,34 @@
 flag.py
 
 Draws the Dominican Republic flag in color on a tkinter Canvas widget.
-This is a condensed version of the previous flag.py script
+This is a condensed version of the previous flag.py script, 
+including the drawPixel function
 
 '''
 
 import tkinter
 import sys
 
-#dimensions of entire flag, in pixels
-
 cross_width = 60
 height = 6 * cross_width
 width = int(height * 3/2)
 
-#The root widget is the window that will contain everything we draw.
 root = tkinter.Tk()
 root.title('Dominican Republic Flag')
 root.geometry(str(width) + 'x' + str(height))
 
-#highlightthickness = 0 allows the canvas to occupy the entire root.
-#also creates a white background
 canvas = tkinter.Canvas(root, highlightthickness = 0, background = '#ffffff')
+
+
+def drawPixel(x, y, color):
+
+    assert isinstance(x, int) and isinstance(y, int) and isinstance(color, str)
+    canvas.create_rectangle(x, y, x + 1, y + 1, width = 0, fill = color)
+
+#red color
+red = '#CE1126'
+#blue color
+blue = '#002D62'
 
 y = 0
 while y < height:
@@ -32,16 +39,18 @@ while y < height:
 
         #top left & bottom right rectangles (dark blue)
         if x < width * .444 and y < 2.5 * cross_width \
-           or x > width * .555 and y > 3.5 * cross_width: 
-            canvas.create_rectangle(x, y, x + 1, y + 1, width = 0, fill = '#002D62')           
+           or x > width * .555 and y > 3.5 * cross_width:
+            drawPixel(x, y, blue)
 
-        #bottom left & top right rectangles (red)           
+        #bottom left & top right rectangles (red)                   
         elif x < width * .444 and y > 3.5 * cross_width \
             or  x > width * .555 and y < 2.5 * cross_width:
-            canvas.create_rectangle(x, y, x + 1, y + 1, width = 0, fill = '#CE1126')
+            drawPixel(x, y, red)
 
         x += 1
     y += 1
+
+
 
 #Make the canvas visible by packing it into the root.
 canvas.pack(expand = tkinter.YES, fill = "both")
