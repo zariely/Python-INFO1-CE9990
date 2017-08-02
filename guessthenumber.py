c08798b9254a8bffc5b1984665c895a618e1e244
zariely <zarielyg@gmail.com>
2017-08-02 17:26:54 -0400

    Update guessthenumber.py

--- guessthenumber.py
+++ guessthenumber.py
@@ -9,40 +9,46 @@ import sys
 import random
 
 def guessTheNumber():
-    secretNumber = random.randint(1, 10)
-    guesses = []  # start with empty list
+    while True:
+        secretNumber = random.randint(1, 10)
+        guesses = set()  # start with empty list
 
-    print("Welcome to Guess the Number!\nTry to figure out what the secret number is!")
+        print("Welcome to Guess the Number!")
+        print("Try to figure out what the secret number is!")
 
-    while len(guesses) < 5:
-        try:
-            guess = int(input("\nGuess a number between 1 and 10: "))
-        except ValueError:
-            print("{} is not a number!".format(guess))
-        else:
-            if guess == secretNumber:
-                print("You got it! My number was {}.".format(secretNumber))
-                break
-            elif guess in guesses:
-                print("You already guessed that number!\n")
-            #gives users a little hint.
-            elif guess < secretNumber:
-                print("My number is higher than {}.".format(guess))
-            elif guess > secretNumber:
-                print("My number is lower than {}.".format(guess))
-
+        while len(guesses) < 5:
+            try:
+                print()
+                guess = int(input("Guess a number between 1 and 10: "))
+            except ValueError:
+                print("{} is not a number!".format(guess))
             else:
-                print("That's not it!")
-            guesses.append(guess) #adds number to the list
-    else:
-        print("\nSorry, you lost. My number was {}.".format(secretNumber))
+                if guess == secretNumber:
+                    print("You got it! My number was {}.".format(secretNumber))
+                    break
+                if guess in guesses:
+                    print("You already guessed that number!")
+                    print()
+                #gives users a little hint.
+                elif guess < secretNumber:
+                    print("My number is higher than {}.".format(guess))
+                elif guess > secretNumber:
+                    print("My number is lower than {}.".format(guess))
 
-    play_again = input("Would you like to play again? Y/N: ")
-    if play_again.lower() == 'y':
-        guessTheNumber()
-    else:
-        print("Ok, bye!")
-        sys.exit(0)
+                else:
+                    print("That's not it!")
+                guesses.add(guess) #adds number to the list
+        else:
+            print()
+            print("Sorry, you lost. My number was {}.".format(secretNumber))
+            print()
+
+        playAgain = input("Would you like to play again? Y/N: ")
+        if playAgain.lower() == 'y':
+            break #check this
+        else:
+            print("Ok, bye!")
+            sys.exit(0)
 
 guessTheNumber()
 
